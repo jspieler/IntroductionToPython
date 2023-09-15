ba39cd8ee4af747cd4a09df5885a2889c9ae6b29
jspieler <75164246+jspieler@users.noreply.github.com>
2023-09-15 19:31:56 +0200

    Added first exercise

---
+++ exercises/exercise_1.py
@@ -0,0 +1,16 @@
+"""First Python exercise on data types, variables and operations.
+
+The first task is to calculate the power consumption of a vehicle
+driving at a constant speed of 80 kilometers per hour.
+The vehicle has a mass of 1500 kg, a drag coefficient of 0.31 and 
+a cross-sectional area of 1.97 m². The rolling resistance coefficient 
+is 0.015.
+
+Following other assumptions are made:
+- The vehicle drives on level ground at a constant velocity.
+- It's windless and the temperature is 20°C.
+- The powertrain efficiency is ideal and there is no wheelspin.
+
+"""
+
+# implement your solution here
