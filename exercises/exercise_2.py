4a79bea6d5d86a52684deb57f3a3faf71c7bb017
jspieler <75164246+jspieler@users.noreply.github.com>
2023-09-15 19:47:52 +0200

    Added second exercise

---
+++ exercises/exercise_2.py
@@ -0,0 +1,11 @@
+"""Second Python exercise on conditions, loops and data structures.
+
+Your next task is to calculate the power consumption of a fleet of
+vehicles under different environmental conditions. Choose a proper
+representation of the data.
+
+Determine if a vehicle is energy-efficient based on a threshold of
+10 kilowatts.
+"""
+
+# implement your solution here
