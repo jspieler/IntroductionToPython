22f3efc0012cf34988bdc4876658e9ed36e34187
jspieler <75164246+jspieler@users.noreply.github.com>
2023-09-16 08:58:41 +0200

    Added fourth exercise

---
+++ exercises/exercise_4.py
@@ -0,0 +1,8 @@
+"""Fourth exercise on exceptions, debugging, testing and the standard library.
+
+This time the vehicle data are given by JSON files. Read in all the data, but 
+keep in mind that the number of files may vary. You can use your previous code 
+and extend it if needed. Apply the topics mentioned above.
+"""
+
+# implement your solution here
