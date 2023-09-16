fd78a55a0c7891cf2dc8b7b8cb0eb485ebdf6836
jspieler <75164246+jspieler@users.noreply.github.com>
2023-09-16 08:58:08 +0200

    Added third exercise

---
+++ exercises/exercise_3.py
@@ -0,0 +1,7 @@
+"""Third Python exercise on functions, classes and modules.
+
+The task is basically the same as the previous exercise, but 
+try to apply the concepts you have learned in the meantime.
+"""
+
+# implement your solution here
